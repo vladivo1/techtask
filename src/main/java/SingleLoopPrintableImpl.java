3f168ff25133413fe8c01bd080d76ec671ab4c25
vladivo1 <vla.ivolga@gmail.com>
2022-01-06 14:03:36 +0200

    update

--- src/main/java/SingleLoopPrintableImpl.java
+++ src/main/java/SingleLoopPrintableImpl.java
@@ -1,19 +1,21 @@
 
 public class SingleLoopPrintableImpl implements MatrixPrintable {
-
     public void print(Integer[][] data) {
 
         for (int row = 0, col = 0; row < data.length; ) {
-
-
+            if (data[row].length == 0) {
+                row++;
+                continue;
+            }
             System.out.print(data[row][col++] + " ");
-
             if (col == data[row].length) {
                 System.out.println();
                 col = 0;
                 row++;
+                }
             }
+        System.out.println("You used Single Loop");
         }
     }
-}
+
 
